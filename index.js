aa90267197e6ed6a9fe5c0ef8bc0205a2688adb4
ruthjelimo <ruthjmimo@gmail.com>
2022-01-27 15:16:21 +0300

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+// console.dir(document);
+console.dir(document.URL);
+console.dir(document.title);
+document.title=10;
+console.log(document.doctype);
+console.log(document.all);
+var headerOne = document.getElementById("head");
+headerOne.style.textDecoration="underline";
